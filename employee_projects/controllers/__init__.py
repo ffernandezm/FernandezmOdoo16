cb97fbd9281acd736d5ea048a500f1f79c34eb7e
ffernandezm <108841126+ffernandezm@users.noreply.github.com>
2024-12-08 22:35:57 -0500

    Delete employee_projects directory

--- employee_projects/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import portal_project
-from . import portal
--- employee_projects/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import test_portal_project
-from . import test_portal
-from . import test_hr_employee
